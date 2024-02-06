0f4b58771bc9c81009e94a27a93535ffe37ca47d
ChaoticGoose <camerondjfairlie@gmail.com>
2024-02-06 11:59:11 +1100

    Updated Cells

--- cells/cells.py
+++ cells/cells.py
@@ -1,7 +1,7 @@
-import tabulate
+import csv
 def cells(passes, n):
-    cells = [{i: False for i in range(1,n+1)}]
-    for i in range(0, passes):
+    cells = [{i: False for i in range(1,n+1)}] # Set every cell to unlocked initally
+    for i in range(0, passes-1): 
         cells.append(dict())
         for cell in cells[i]:
             if cell % (i+2) == 0:
@@ -13,8 +13,11 @@ def cells(passes, n):
                 cells[i+1][cell] = cells[i][cell]
     return cells
 
-
-cells_list = cells(3, 7)
+# CSV output writer
+cells_list = cells(500, 500)
 header = cells_list[0].keys()
-rows = [x.values() for x in cells_list]
-print(tabulate.tabulate(rows, header, tablefmt="simple"))
+with open('cells.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, header)
+    dict_writer.writeheader()
+    dict_writer.writerows(cells_list)
+print("csv written to ./cells.csv")
